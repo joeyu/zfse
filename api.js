d215a4d9aa99c63307c33859f28e83ba694184e6
Zhou Yu <nap.zhou.yu@gmail.com>
2014-11-22 20:11:19 +0800

    Created gh-pages

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "zfse"
+    ],
+    "modules": [
+        "zfse"
+    ],
+    "allModules": [
+        {
+            "displayName": "zfse",
+            "name": "zfse",
+            "description": "Zhou's File System Extension extends the functionality of the 'fs' module of node.js."
+        }
+    ]
+} };
+});
